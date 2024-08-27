53cd5e799643e9cd5928fe734b0e534955d150d4
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-08-27 12:55:11 +0500

    Добавлено контекстное меню и всплывающие окна

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var toolbarMain: androidx.appcompat.widget.Toolbar
 
     private lateinit var firstOperandET: EditText
     private lateinit var secondOperandET: EditText
@@ -21,9 +27,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var operatorDivideBTN: Button
 
     private lateinit var resultTV: TextView
-
-    private lateinit var resetBTN: Button
-    private lateinit var exitBTN: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +39,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             insets
         }
 
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "Калькулятор"
+        toolbarMain.subtitle = "версия 2"
+        toolbarMain.setLogo(R.drawable.ic_calculate)
+
+
         firstOperandET = findViewById(R.id.firstOperandET)
         secondOperandET = findViewById(R.id.secondOperandET)
 
@@ -44,37 +54,55 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         operatorMultiplyBTN = findViewById(R.id.operatorMultiplyBTN)
         operatorDivideBTN = findViewById(R.id.operatorDivideBTN)
 
-        exitBTN = findViewById(R.id.exitBTN)
-        resetBTN = findViewById(R.id.resetBTN)
-
         resultTV = findViewById(R.id.resultTV)
 
         operatorPlusBTN.setOnClickListener(this)
         operatorMinusBTN.setOnClickListener(this)
         operatorMultiplyBTN.setOnClickListener(this)
         operatorDivideBTN.setOnClickListener(this)
-        resetBTN.setOnClickListener(this)
-        exitBTN.setOnClickListener(this)
 
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.resetMenuMain -> {
+                firstOperandET.text.clear()
+                secondOperandET.text.clear()
+                resultTV.text = "Результат"
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Работа завершена",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onClick(view: View) {
-        var check = true
-
-
-        var result = 0.0
-
         if (firstOperandET.text.isEmpty() || secondOperandET.text.isEmpty()) {
-
             resultTV.text = "Введите значения операндов"
             return
-
         }
 
         var first = firstOperandET.text.toString().toDouble()
         var second = secondOperandET.text.toString().toDouble()
 
+        var result = 0.0
 
         try {
             result = when (view.id) {
@@ -82,23 +110,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 R.id.operatorMinusBTN -> Operation(first, second).difference()
                 R.id.operatorMultiplyBTN -> Operation(first, second).multiply()
                 R.id.operatorDivideBTN -> Operation(first, second).division()
-                R.id.resetBTN -> {
-                    firstOperandET.text.clear()
-                    secondOperandET.text.clear()
-                    check = false
-                    0.0
-                }
-                R.id.exitBTN -> {
-                    finish()
-                    0.0
-                }
                 else -> 0.0
             }
         } catch (e: ArithmeticException) {
             resultTV.text = "На ноль делить нельзя"
         }
 
-        if (!check) resultTV.text = "Результат" else resultTV.text = result.toString()
+        resultTV.text = result.toString()
 
     }
 }
